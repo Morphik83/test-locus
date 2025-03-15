9638456cd3c7e3352362c3c6a5516bea234a8f6c
morphik <balazy.maciek@gmail.com>
2025-03-15 11:38:19 +0100

    Add single user test scenario with basic flow

--- test_singleuser.py
+++ test_singleuser.py
@@ -1,1 +1,16 @@
 # test_singleuser.py
+from locust import HttpUser, task, between
+
+class SingleUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def basic_flow(self):
+        # Login
+        self.client.post("/login", json={"username": "test_user", "password": "test_pass"})
+        
+        # Check balance
+        self.client.get("/balance")
+        
+        # Logout
+        self.client.post("/logout")
